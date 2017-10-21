35e77b1803bc933cf8e6bda7f09ac44d4587bc00
Tom Johnson <tom@dp.la>
2017-10-21 09:45:17 -0700

    Test that restricted ETDs cannot be downloaded
    Non-logged in users should not be able to download restricted ETDs.

--- spec/features/access_etd_spec.rb
+++ spec/features/access_etd_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 include Warden::Test::Helpers
 
 RSpec.feature 'Access an Etd', :perform_enqueued, js: false do
-  let(:etd) { FactoryGirl.create(:public_etd, pdf: pdf) }
-  let(:pdf) { FactoryGirl.create(:pdf_upload) }
+  let(:etd)         { FactoryGirl.create(:public_etd, pdf: pdf) }
+  let(:pdf)         { FactoryGirl.create(:pdf_upload) }
+  let(:private_etd) { FactoryGirl.create(:etd, pdf: pdf) }
 
   # Only enqueue the ingest job, not charactarization.
   before { ActiveJob::Base.queue_adapter.filter = [IngestJob] }
@@ -13,5 +14,10 @@ RSpec.feature 'Access an Etd', :perform_enqueued, js: false do
     click_link "Download \"#{etd.representative.first_title}\""
 
     expect(page.response_headers['Content-Disposition']).to include 'attachment'
+  end
+
+  scenario 'downloading restricted' do
+    visit "concern/etds/#{private_etd.id}"
+    expect(page).not_to have_content 'Download'
   end
 end
