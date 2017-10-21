806bee665be6e8a0c5bcc0cacf8358b0ad1275ff
Tom Johnson <tom@dp.la>
2017-10-21 11:35:10 -0700

    Test 'required' and validation status of `ETD` attribute `:title`
    Partially addresses #60.

--- spec/forms/hyrax/etd_form_spec.rb
+++ spec/forms/hyrax/etd_form_spec.rb
@@ -3,7 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe Hyrax::EtdForm do
-  it "has tests" do
-    skip "Add your tests here"
+  subject(:form) { described_class.new(pdf, nil, nil) }
+  let(:pdf)      { FactoryGirl.build(:etd) }
+
+  describe '#required_fields' do
+    it 'requires title' do
+      expect(form.required_fields).to include :title
+    end
   end
 end
