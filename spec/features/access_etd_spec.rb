f62b5403e27c3f7ce407e057d8af1ab82ff7929d
Tom Johnson <tom@dp.la>
2017-10-21 10:26:20 -0700

    Test search for unrestricted ETDs
    Run a search and ensure that an unrestricted item exists.

--- spec/features/access_etd_spec.rb
+++ spec/features/access_etd_spec.rb
@@ -1,23 +1,34 @@
 require 'rails_helper'
 include Warden::Test::Helpers
 
-RSpec.feature 'Access an Etd', :perform_enqueued, js: false do
+RSpec.feature 'Access an Etd', js: false do
   let(:etd)         { FactoryGirl.create(:public_etd, pdf: pdf) }
   let(:pdf)         { FactoryGirl.create(:pdf_upload) }
   let(:private_etd) { FactoryGirl.create(:etd, pdf: pdf) }
 
-  # Only enqueue the ingest job, not charactarization.
-  before { ActiveJob::Base.queue_adapter.filter = [IngestJob] }
+  scenario 'searching submitted ETDs' do
+    visit '/'
 
-  scenario 'downloading' do
-    visit      "concern/etds/#{etd.id}"
-    click_link "Download \"#{etd.representative.first_title}\""
+    fill_in 'Search Hyrax', with: etd.first_title
+    click_button 'Go'
 
-    expect(page.response_headers['Content-Disposition']).to include 'attachment'
+    expect(page).to have_css("li#document_#{etd.id}")
   end
 
-  scenario 'downloading restricted' do
-    visit "concern/etds/#{private_etd.id}"
-    expect(page).not_to have_content 'Download'
+  context 'with ingested file', :perform_enqueued do
+    # Only enqueue the ingest job, not charactarization.
+    before { ActiveJob::Base.queue_adapter.filter = [IngestJob] }
+
+    scenario 'downloading' do
+      visit      "concern/etds/#{etd.id}"
+      click_link "Download \"#{etd.representative.first_title}\""
+
+      expect(page.response_headers['Content-Disposition']).to include 'attachment'
+    end
+
+    scenario 'downloading restricted' do
+      visit "concern/etds/#{private_etd.id}"
+      expect(page).not_to have_content 'Download'
+    end
   end
 end
