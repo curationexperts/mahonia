be88e538f62c5d00460b5ca98e769af9a6b24ed9
Tom Johnson <tom@dp.la>
2017-10-23 12:49:07 -0700

    Add rights_note to ETDs
    Create a `CoreMetadata` schema to hold generic OHSU data elements. Include this
    schema in `Etd` and add a `rights_note` property.

--- app/models/etd.rb
+++ app/models/etd.rb
@@ -10,6 +10,8 @@ class Etd < ActiveFedora::Base
 
   self.human_readable_type = 'Etd'
 
+  apply_schema Schemas::CoreMetadata
+
   # This must be included at the end, because it finalizes the metadata
   # schema (by adding accepts_nested_attributes)
   include ::Hyrax::BasicMetadata
