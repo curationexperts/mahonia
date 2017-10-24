cfa6e47741fa777a3771e2dc38c035724a0b6807
Tom Johnson <tom@dp.la>
2017-10-24 13:57:05 -0700

    Make `have_editable_property` matcher more robust
    Add error messaging and better handling of single value conditions to the
    `have_editable_property` matcher. The sytax is extended to allow
    `with_predicate` chaining. The old style should eventually be removed.

--- spec/support/matchers/have_editable_property.rb
+++ spec/support/matchers/have_editable_property.rb
@@ -1,11 +1,25 @@
 RSpec::Matchers.define :have_editable_property do |property, predicate|
   match do |model|
+    # @todo deprecate two-argument call style in favor chaining
+    predicate ||= chain_predicate
+    predicate = RDF::URI.intern(predicate)
+
     values = ['Comet in Moominland', 'Moomin Midwinter']
     values = values.first if @single
+
+    config = model.class.properties.fetch(property.to_s) do
+      @missing_property = true
+      return false
+    end
 
     expect { model.public_send("#{property}=", values) }
       .to change { Array(model.public_send(property)) }
       .to contain_exactly(*values)
+
+    return true unless predicate
+
+    @actual_predicate = config.predicate
+    return false unless @actual_predicate == predicate
 
     Array(values).each do |value|
       expect(model.resource.statements)
@@ -15,7 +29,19 @@ RSpec::Matchers.define :have_editable_property do |property, predicate|
     true
   end
 
+  chain :with_predicate, :chain_predicate
+
   chain :as_single_valued do
     @single = true
+  end
+
+  failure_message do |model|
+    msg = "expected #{model.class} to have an editable property `#{property}`"
+    msg += " with predicate #{predicate.to_base}." if predicate
+
+    return msg + "\n\tNo property for `#{property}` was found." if @missing_property
+
+    msg += "\n\tThe configured predicate was #{@actual_predicate.to_base}" unless @actual_predicate == predicate
+    msg
   end
 end
