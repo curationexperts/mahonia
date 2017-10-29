cb6fc5714efb11d0a2c01bce885271d4a9569504
Bess Sadler <bess@curationexperts.com>
2017-10-29 12:57:24 -0400

    Add hydra_role_management per instructions at https://github.com/samvera/hyrax/wiki/Making-Admin-Users-in-Hyrax

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,7 +3,9 @@ class Ability
 
   include Hyrax::Ability
   self.ability_logic += [:everyone_can_create_curation_concerns]
-
+  if current_user.admin?
+    can [:create, :show, :add_user, :remove_user, :index, :edit, :update, :destroy], Role
+  end
   # Define any customized permissions here.
   def custom_permissions
     # Limits deleting objects to a the admin user
