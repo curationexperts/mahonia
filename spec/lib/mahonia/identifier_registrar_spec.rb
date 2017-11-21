b636c9619ce6bfb517979631f3d7631ec2e1baa0
Tom Johnson <tom@dp.la>
2017-11-20 16:11:25 -0800

    Test abstract method `Mahonia::IdentifierRegistrar#register!`

--- spec/lib/mahonia/identifier_registrar_spec.rb
+++ spec/lib/mahonia/identifier_registrar_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Mahonia::IdentifierRegistrar do
+  subject(:registrar) { described_class.new(builder: :NOT_A_REAL_BUILDER) }
+
   it_behaves_like 'an IdentifierRegistrar'
+
+  it 'is abstract' do
+    expect { registrar.register!(object: :NOT_A_REAL_OBJECT) }
+      .to raise_error NotImplementedError
+  end
 end
