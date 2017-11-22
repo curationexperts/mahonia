ad91dab10515ba183a2fe1ee9a83f6284404466d
Bess Sadler <bess@curationexperts.com>
2017-11-22 08:26:12 -0500

    Fix reference to SIDEKIQ_CONCURRENCY Use ERB to interpret environment variables

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :assets_prefix, "#{shared_path}/public/assets"
 
 SSHKit.config.command_map[:rake] = 'bundle exec rake'
 
-set :branch, ENV['REVISION'] || ENV['BRANCH_NAME'] || 'master'
+set :branch, ENV['REVISION'] || ENV['BRANCH_NAME'] || ENV['BRANCH'] || 'master'
 
 append :linked_dirs, "log"
 append :linked_dirs, "public/assets"
