834bca452e87471b42572584a5339eb632e1be6a
Tom Johnson <tom@dp.la>
2018-01-03 13:08:08 -0800

    Revert "editing date field, predicate is now verbatimEventDate and label is 'EDTF Date'"
    The predicate was correct before, we need a follow up to reinstate the new
    label.
    
    This reverts commit 8598f9ef71bace7275dc66f65c60cba212f7b208.

--- app/models/schemas/core_metadata.rb
+++ app/models/schemas/core_metadata.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 module Schemas
   class CoreMetadata < ActiveTriples::Schema
-    property :date,        predicate: RDF::Vocab::DWC.verbatimEventDate
+    property :date,        predicate: RDF::Vocab::DC11.date
     property :date_label,  predicate: RDF::Vocab::DWC.verbatimEventDate
     property :keyword,     predicate: RDF::Vocab::SCHEMA.keywords
     property :rights_note, predicate: RDF::Vocab::DC11.rights
--- spec/views/hyrax/base/_form_metadata.html.erb_spec.rb
+++ spec/views/hyrax/base/_form_metadata.html.erb_spec.rb
@@ -68,7 +68,7 @@ RSpec.describe 'hyrax/base/_form_metadata.html.erb', type: :view do
       expect(page)
         .to have_multivalued_field(:date)
         .on_model(work.class)
-        .with_label 'EDTF Date'
+        .with_label 'Date'
     end
 
     it 'has date labels' do
