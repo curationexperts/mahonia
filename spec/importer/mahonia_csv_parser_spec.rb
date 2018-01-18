4de8db143225813d0b5ffd0b95db18c9d75542ac
Tom Johnson <tom@dp.la>
2018-01-18 12:29:02 -0800

    Test import parser records for invalid CSV case
    We want an empty parser in case of invalid CSV, so validators can consume the
    empty record stream instead of running into errors.

--- spec/importer/mahonia_csv_parser_spec.rb
+++ spec/importer/mahonia_csv_parser_spec.rb
@@ -11,6 +11,14 @@ RSpec.describe MahoniaCsvParser do
     it 'skips records with no values' do
       expect(parser.records.count).to eq 3
     end
+
+    context 'with invalid csv' do
+      let(:csv_path) { 'spec/fixtures/malformed.csv' }
+
+      it 'is empty' do
+        expect(parser.records.to_a).to be_empty
+      end
+    end
   end
 
   describe '#validate' do
