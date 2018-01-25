4101898b870ec323997b28f30c15a12d020e4b5f
Bess Sadler <bess@curationexperts.com>
2018-01-25 09:55:41 -0500

    Add omniauth required fields to User model
    Add uid field and index it: uid will now be the primary identifier
    Add provider field to record which authentication method created a given user
    Add uid field to User factory

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171215182415) do
+ActiveRecord::Schema.define(version: 20180125145309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -519,8 +519,11 @@ ActiveRecord::Schema.define(version: 20171215182415) do
     t.binary   "zotero_token"
     t.string   "zotero_userid"
     t.string   "preferred_locale"
+    t.string   "provider"
+    t.string   "uid"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.index ["uid"], name: "index_users_on_uid", using: :btree
   end
 
   create_table "version_committers", force: :cascade do |t|
